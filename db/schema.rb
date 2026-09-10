77f22593781f9a66431c6ade0d07756710556a5c
Bruno Schumacher <brunoschumacherfsouza@gmail.com>
2026-09-10 04:13:55 -0300

    chore(db): update database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_10_061704) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_10_063640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -46,6 +46,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_10_061704) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "user_imports", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.jsonb "error_messages"
@@ -71,5 +80,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_10_061704) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "sessions", "users"
   add_foreign_key "user_imports", "users"
 end
