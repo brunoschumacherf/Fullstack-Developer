3ced6fa9679724a39c1e4714ee362573cffda119
Bruno Schumacher <brunoschumacherfsouza@gmail.com>
2026-09-11 02:06:33 -0300

    fix: configure Active Record encryption from environment

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,16 @@ Bundler.require(*Rails.groups)
 
 module UserManagementApp
   class Application < Rails::Application
+
+    config.active_record.encryption.primary_key =
+      ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+
+    config.active_record.encryption.deterministic_key =
+      ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+
+    config.active_record.encryption.key_derivation_salt =
+      ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
+
     config.load_defaults 8.1
     config.autoload_lib(ignore: %w[assets tasks])
     config.i18n.available_locales = %i[pt-BR en]
